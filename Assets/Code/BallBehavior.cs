42efc48fb3422f92cdf2242e8f8f6df6b4f0d4f0
SuperFireBomber <122469311+SuperFireBomber@users.noreply.github.com>
2025-01-28 21:02:43 -0800

    add ball, ground, add the ball code

---
+++ Assets/Code/BallBehavior.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Debug.Log("Calling the Update method");
+    }
+}
